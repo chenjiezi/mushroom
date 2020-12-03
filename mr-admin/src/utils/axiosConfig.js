61c4635243c7db442a721a10db1078ba8b6d70f3
chenjiezi <892774940@qq.com>
2020-12-03 16:57:20 +0800

    axios配置更改

--- mr-admin/src/utils/axiosConfig.js
+++ mr-admin/src/utils/axiosConfig.js
@@ -1,16 +1,33 @@
 import axios from 'axios'
+import { MessageBox, Message } from 'element-ui'
+import store from '@/store'
+import { getToken } from '@/utils/auth'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  // withCredentials: true, // send cookies when cross-domain requests
-  timeout: 5000, // request timeout
+  baseURL: process.env.VUE_APP_BASE_API,
+  timeout: 5000,
   headers: {
     'Content-Type': 'application/json;charset=utf-8;',
   }
-
 })
 
+// 请求拦截器
+service.interceptors.request.use(
+  config => {
+    // 请求时，带上token
+    if (store.getters.token) {
+      config.headers['Authorization'] = `Bearer ${getToken()}`
+    }
+    return config
+  },
+  error => {
+    console.log(error)
+    return Promise.reject(error)
+  }
+)
+
+// 相应拦截器
 service.interceptors.response.use(
   response => {
     const res = response.data
